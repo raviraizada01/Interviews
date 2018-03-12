8eda63f14e5c0158b996c8ee7533b0402c7e37a3
raviraizada01 <raviraizadagm@gmail.com>
2018-03-12 13:56:57 +0530

    Add files via upload

---
+++ nestedArray/my.js
@@ -0,0 +1,18 @@
+var a = [1,2,[3,4,[8,9,[1,2],[6,7]]]];
+newArr = [];
+function flattenArray(arr){
+
+for(var i=0; i<arr.length;i++)
+{
+if(typeof arr[i] == "object")
+{
+flattenArray(arr[i]);
+}
+else{
+newArr.push(arr[i]);
+}
+}
+return newArr;
+}
+
+console.log(flattenArray(a));
